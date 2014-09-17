efafe8f76148279fd49355de50e08c2e7873ca71
zhzenghui <zhzenghui@gmail.com>
2014-09-18 01:41:15 +0800

    add afnetwork

--- GetM3u/ZHViewController.m
+++ GetM3u/ZHViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ZHViewController.h"
+#import "AFNetworking/AFNetworking.h"
 
 @interface ZHViewController ()
 
@@ -14,16 +15,47 @@
 
 @implementation ZHViewController
 
+- (void)gethtmlM3u8
+{
+    NSURL *url = [NSURL URLWithString:@"http://v.youku.com/v_show/id_XNzcyMDAxMzQw.html"];
+
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc]
+                                         initWithRequest:request];
+//    operation.responseSerializer = [AFXMLParserResponseSerializer serializer];
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+
+//        NSString *str = [NSString stringWithCString:responseObject  encoding:<#(NSStringEncoding)#>]
+        
+        
+        NSString* newStr = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+        NSLog(@"%@", newStr);
+
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"%@", error);
+    }];
+    [operation start];
+    
+
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
-    _webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
+//    _webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
+//    
+//    
+//    NSURL *url = [NSURL URLWithString:@"http://v.youku.com/v_show/id_XNzcyMDAxMzQw.html"];
+//    _webView.delegate = self;
+//    [_webView loadRequest:[NSURLRequest  requestWithURL:url  ]];
+//    
     
     
-    NSURL *url = [NSURL URLWithString:@"http://v.youku.com/v_show/id_XNzcyMDAxMzQw.html"];
-    _webView.delegate = self;
-    [_webView loadRequest:[NSURLRequest  requestWithURL:url  ]];
+    [self gethtmlM3u8];
 }
 
 - (void)didReceiveMemoryWarning
@@ -36,15 +68,60 @@
 -(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
 {
     
+    NSLog(@"%@",      webView.request);
     
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView
 {
-    NSString *html =  [webView stringByEvaluatingJavaScriptFromString:@"document.body.innerHTML"];
+//    NSString *html =  [webView stringByEvaluatingJavaScriptFromString:@"document.body.innerHTML"];
 
     
-    NSLog(@"%@", html);
+
+           NSString *str = [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByTagName('video')[0].getAttribute('src');"];
+    
+//    if (![str isEqualToString:@""])
+//    {
+//          
+////                         NSString *tempStr =str;
+////                         //NSLog(@"======%@",tempStr);
+////                        NSRange range = [ str rangeOfString:@"http://"];
+////                         if (range.length==0){
+////                                range = [videolinkStr rangeOfString:@"youku.com"];
+////                                if (range.length>0){
+////                                        str = [@"http://v.youku.com" stringByAppendingString:tempStr];
+////                                    }
+////                             }
+//        
+//    
+//     
+//    }
+//else{
+//                   BOOL sourcemode = [self isSourceMode];
+//                    if (sourcemode){
+//                           str = [webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByTagName('source')[0].getAttribute('src')"];
+//                        }
+//                }
+//         if (IfPass == NO) {//设置BOOL类型的IfPass主要目的避免重复加载获取。
+//      
+//               if (str && ![str isEqualToString:@""]) {
+//                        [webView stopLoading];
+//                        if ([delegate respondsToSelector:@selector(authorizeWebView:didReceiveAuthorizeCode:)])
+//                         {
+//                                  NSLog(@"地址信息=======%@",str);
+//                                  IfPass = YES;
+//                                 [delegate authorizeWebView:self didReceiveAuthorizeCode:str];
+//                              }
+//                   }
+//           }else if (IfPass == YES){
+//                     [webView stopLoading];
+//                     //NSLog(@"地址信息=======%@",str);
+//              }
+
+NSLog(@"Web获取视频地址信息=======%@",str);
+
+
+
 }
 
 - (void)webViewDidStartLoad:(UIWebView *)webView
